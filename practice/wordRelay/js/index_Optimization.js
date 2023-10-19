c4a87bff7a6d77a0000d140ed1c3474670f14f72
i12free011@gmail.com <i12free011@gmail.com>
2023-10-19 13:11:59 +0900

    c231019

--- practice/wordRelay/js/index_Optimization.js
+++ practice/wordRelay/js/index_Optimization.js
@@ -9,15 +9,25 @@ let newWord = ""; // 입력 단어
 const people = parseInt(prompt("참가 인원을 정수로 작성해주세요. (ex. 5)"));
 
 function handleWordBtnClick() {
-  if (existWord === "") {
-    existWord = newWord;
-    // existWordSpan.innerText = existWord;
-    existWordSpan.textContent = existWord;
-    wordInput.value = "";
-    wordInput.focus();
+  let newWord = wordInput.value;
+  if (existWord === "" || newWord[0] == existWord[existWord.length -1]) {
+    if (newWord.length !== 1) {
+      let currentOrder = parseInt(orderSpan.textContent);
+      if (currentOrder === people) {
+        orderSpan.textContent = "1";
+      } else {
+        orderSpan.textContent = currentOrder + 1;
+      }
+
+      existWord = newWord;
+      existWordSpan.textContent = existWord;
+      wordInput.value = "";
+      wordInput.focus();
+    } else {
+      resultH1.textContent = "올바르지 않은 단어입니다. 실패!"
+    }
 
   } else if (existWord != "") {
-    if (newWord[0] == existWord[existWord.length -1]) {
       if (newWord.length !== 1) {
         let currentOrder = parseInt(orderSpan.textContent);
         if (currentOrder === people) {
@@ -35,15 +45,8 @@ function handleWordBtnClick() {
         alert("단어를 한 글자 이상 입력해주세요.")
         wordInput.focus();
       }
-    } else {
-      resultH1.textContent = "올바르지 않은 단어입니다. 실패!"
-    }
 
   }
 }
-function handleWordInputWrite(event) {
-  newWord = event.target.value;
-}
 
 wordBtn.addEventListener("click", handleWordBtnClick)
-wordInput.addEventListener("input",handleWordInputWrite)
---
+++ study/08. inputOutputMethod/inputOutputMethod.js
@@ -0,0 +1,13 @@
+prompt();
+prompt("What's your name?");
+const people = prompt("참가 인원을 정수로 작성해주세요.", "ex) 5");
+
+connfirm();
+var yesOrNo = confirm("약관에 동의하시겠습니까?");
+console.log(yesOrNo);
+
+alert();
+alert("Hi, JS!");
+
+console.log("123");
+console.log(1, "+", 3, "=", 4);
